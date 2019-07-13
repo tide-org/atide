4c80c212cf2e30bf6248f200ed5f94121b6b2e6e
Willem van Ketwich <willvk@gmail.com>
2019-07-13 13:34:44 +1000

    add tes for python

--- spec/process-controller-spec.js
+++ spec/process-controller-spec.js
@@ -3,8 +3,12 @@
 import ProcessController from '../lib/process-controller';
 
 describe('ProcessController', () => {
-  it('returns an expected result', () => {
+  it('returns hello from the command', () => {
     processController = new ProcessController()
-    processController.runQuickProcess("echo hello").then( res => expect(res).toBe('hello'));
+    processController.runQuickProcess("echo hello").then( res => expect(res.trim()).toBe('hello'));
+  }),
+  it('returns hello from python', () => {
+    processController = new ProcessController()
+    processController.runQuickProcess("python -c 'print(\"hello\")'").then( res => expect(res.trim()).toBe('hello'));
   });
 });
