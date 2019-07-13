f877c7e60db9835254e4f90de90c5ec6f9e5dfeb
Willem van Ketwich <willvk@gmail.com>
2019-07-13 16:45:08 +1000

    fix tests

--- spec/process-controller-spec.js
+++ spec/process-controller-spec.js
@@ -3,12 +3,21 @@
 import ProcessController from '../lib/process-controller';
 
 describe('ProcessController', () => {
+
+  describe('', () => {
+
   it('returns hello from the command', () => {
     processController = new ProcessController()
     processController.runQuickProcess("echo hello").then( res => expect(res.trim()).toBe('hello'));
-  }),
-  it('returns hello from python', () => {
-    processController = new ProcessController()
-    processController.runQuickProcess("python -c 'print(\"hello\")'").then( res => expect(res.trim()).toBe('hello'));
   });
+
+});
+
+  describe('', () => {
+    it('returns hello from the command', () => {
+      processController = new ProcessController()
+      processController.runQuickProcess("python -c 'print(\"hello\")'").then( res => expect(res.trim()).toBe('hello'));
+    });
+  });
+
 });
