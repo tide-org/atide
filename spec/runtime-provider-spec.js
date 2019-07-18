44565f90007f911aba19588af622e931141719cf
Willem van Ketwich <willvk@gmail.com>
2019-07-18 21:36:50 +1000

    check for instance type

--- spec/runtime-provider-spec.js
+++ spec/runtime-provider-spec.js
@@ -1,10 +1,18 @@
 'use babel';
 
+import Runtime from '../lib/atom-script/runtime';
 import RuntimeProvider from '../lib/runtime-provider';
 
 describe('', () => {
   it('returns an object that is not undefined', () => {
       runtime = RuntimeProvider.build();
       expect(runtime).not.toBeUndefined();
+  });
+});
+
+describe('', () => {
+  it('returns an object of type Runtime', () => {
+      runtime = RuntimeProvider.build();
+      expect(runtime instanceof Runtime).toBeTruthy()
   });
 });
