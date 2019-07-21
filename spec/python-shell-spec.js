2ef23b201dc8423c565973b0590a4fafc7f7ac58
Willem van Ketwich <willvk@gmail.com>
2019-07-21 19:29:34 +1000

    py-script run script file

--- spec/python-shell-spec.js
+++ spec/python-shell-spec.js
@@ -1,11 +1,21 @@
 const { PythonShell } = require('../lib/python-shell');
 
 describe('', () => {
-  it('returns hello from the command', () => {
+  it('pyshell completes without error', () => {
     PythonShell.runString('x=1+1;print(x)', null, function (err) {
       if (err) throw err;
       expect(err).toBe(null);
       console.log('finished');
     });
+  });
+});
+
+describe('', () => {
+  it('pyshell completes without error running a script', () => {
+		PythonShell.run('./my_script.py', null, function (err) {
+			if (err) throw err;
+      expect(err).toBe(null);
+			console.log('finished');
+		});
   });
 });
