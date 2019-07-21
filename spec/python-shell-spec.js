6456fa311d0ad3c3539bbbeaffed8c90bbe9d41e
Willem van Ketwich <willvk@gmail.com>
2019-07-21 19:25:14 +1000

    pyshell initial test working

--- spec/python-shell-spec.js
+++ spec/python-shell-spec.js
@@ -1,23 +1,11 @@
-'use babel';
-
-import PythonShell from 'python-shell';
+const { PythonShell } = require('../lib/python-shell');
 
 describe('', () => {
   it('returns hello from the command', () => {
-		let pyshell = new PythonShell('echo_text.py', {
-				mode: 'text'
-		});
-
-		let output = '';
-		pyshell.stdout.on('data', function (data) {
-				output += ''+data;
-		});
-
-		pyshell.send('hello').send('world').end(function (err) {
-				if (err) return done(err);
-				output.should.be.exactly('hello'+newline+'world'+newline);
-				done();
-		});
-
+    PythonShell.runString('x=1+1;print(x)', null, function (err) {
+      if (err) throw err;
+      expect(err).toBe(null);
+      console.log('finished');
+    });
   });
 });
