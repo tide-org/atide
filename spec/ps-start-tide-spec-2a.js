2c3b6301fb39aac3cb0adb0ff55272c1abe4fd79
Willem van Ketwich <willvk@gmail.com>
2019-08-19 20:25:28 +1000

    add key spec

--- spec/ps-start-tide-spec-2a.js
+++ spec/ps-start-tide-spec-2a.js
@@ -22,23 +22,38 @@ describe('tide async', () => {
     done();
   }, 1000);
 
-  it('starts a python script that runs tide.', (done) => {
+  it('starts a python script that runs tide and has 3 json objects return', (done) => {
     let messageCount = 0;
-
     let pyshell = PythonShell.run('start-tide.py', options);
-
     pyshell.on('message', (message) => {
       if (isJson(message)) {
         messageCount += 1;
       }
     });
-
 		pyshell.end( (err, code, signal) => {
 			if (err) throw err;
       expect(messageCount).toBe(3);
       done();
 		});
+  }, 10000);
 
+  it('starts a python script that runs tide and has the first object return have a command key', (done) => {
+    let messageCount = 0;
+    let jsonObject = {};
+    let pyshell = PythonShell.run('start-tide.py', options);
+    pyshell.on('message', (message) => {
+      if (isJson(message)) {
+          messageCount += 1;
+          if (messageCount == 1) {
+            jsonObject = JSON.parse(message);
+          }
+      }
+    });
+		pyshell.end( (err, code, signal) => {
+			if (err) throw err;
+      expect(Object.keys(jsonObject)[0]).toBe('command');
+      done();
+		});
   }, 10000);
 
   afterEach((done) => {
