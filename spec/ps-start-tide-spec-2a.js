6347df5a6c9867a16b2f557cd198c311de917117
Willem van Ketwich <willvk@gmail.com>
2019-08-19 21:08:35 +1000

    add specs

--- spec/ps-start-tide-spec-2a.js
+++ spec/ps-start-tide-spec-2a.js
@@ -1,5 +1,6 @@
 const { PythonShell } = require('../lib/python-shell');
 
+process.env['TIDE_CONFIG_LOCATION'] = '/Users/willvk/Source/wilvk/tide-plugins/plugins/test_c-stdio/'; // TODO: run in a container
 
 function isJson(str) {
   try {
@@ -90,6 +91,52 @@ describe('tide async', () => {
 		pyshell.end( (err, code, signal) => {
 			if (err) throw err;
       expect(Object.keys(jsonObject["command"])[1]).toBe('value');
+      done();
+		});
+  }, 10000);
+
+  it('starts a python script that runs tide and has the first object return have a command key with an action key with value "set_full_config_dictionary"', (done) => {
+    let messageCount = 0;
+    let jsonObject = {};
+    let pyshell = PythonShell.run('start-tide.py', options);
+    pyshell.on('message', (message) => {
+      if (isJson(message)) {
+          messageCount += 1;
+          if (messageCount == 1) {
+            jsonObject = JSON.parse(message);
+          }
+      }
+    });
+		pyshell.end( (err, code, signal) => {
+			if (err) throw err;
+      expect(jsonObject["command"]["action"]).toBe('set_full_config_dictionary');
+      done();
+		});
+  }, 10000);
+
+  it(`starts a python script that runs tide and has the first object return have 
+      a command key with command.value keys of: 
+      settings,buffers,events,variables,commands `, (done) => {
+    let messageCount = 0;
+    let jsonObject = {};
+    let pyshell = PythonShell.run('start-tide.py', options);
+    pyshell.on('message', (message) => {
+      if (isJson(message)) {
+          messageCount += 1;
+          if (messageCount == 1) {
+            jsonObject = JSON.parse(message);
+          }
+      }
+    });
+		pyshell.end( (err, code, signal) => {
+			if (err) throw err;
+      actionValue = jsonObject["command"]["value"];
+      actionValueKeys = Object.keys(actionValue);
+      expect(actionValueKeys.indexOf('settings')).not.toEqual(-1);
+      expect(actionValueKeys.indexOf('buffers')).not.toEqual(-1);
+      expect(actionValueKeys.indexOf('events')).not.toEqual(-1);
+      expect(actionValueKeys.indexOf('variables')).not.toEqual(-1);
+      expect(actionValueKeys.indexOf('commands')).not.toEqual(-1);
       done();
 		});
   }, 10000);
