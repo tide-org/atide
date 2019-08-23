2fbc396895f36b8ef9a5a795aec6f405a2b26a86
Willem van Ketwich <willvk@gmail.com>
2019-08-24 00:02:29 +1000

    fix request and add tests

--- spec/request-spec.js
+++ spec/request-spec.js
@@ -1,24 +1,40 @@
 const Request = require('../lib/request');
 
+let requestObject = {
+  "command": {
+    "action": "set_full_config_dictionary",
+    "value": {
+      "settings": { "setting_a": "aaaa", "setting_b": "bbbb" },
+      "variables": { "variable_c": "cccc", "variable_d": "dddd" }
+    }
+  },
+  "sender": "tide",
+  "receiver": "editor",
+};
+
 describe('test request class does as expected  ', () => {
 
   it('can have a valid json string passed to it', () => {
-
-      let requestObject = {
-        "command": {
-          "action": "set_full_config_dictionary",
-          "value": {
-            "settings": { "setting_a": "aaaa", "setting_b": "bbbb" },
-            "variables": { "variable_c": "cccc", "variable_d": "dddd" }
-          }
-        },
-        "sender": "tide",
-        "receiver": "editor",
-      };
       let requestString = JSON.stringify(requestObject);
       let request = new Request(requestString);
       let isValidRequest = request.isValidRequest();
       expect(isValidRequest).toBe(true);
+  });
+
+  it('can get the action name', () => {
+      let requestString = JSON.stringify(requestObject);
+      let request = new Request(requestString);
+      let action = request.getAction();
+      expect(action).toBe("set_full_config_dictionary");
+  });
+
+  it('can get the action value', () => {
+      let requestString = JSON.stringify(requestObject);
+      let request = new Request(requestString);
+      let valueObject = request.getValue();
+      let valueKeys = Object.keys(valueObject);
+      expect(valueKeys.indexOf("settings")).not.toBe(-1);
+      expect(valueKeys.indexOf("variables")).not.toBe(-1);
   });
 
 });
