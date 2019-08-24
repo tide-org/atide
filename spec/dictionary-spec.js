a957b8296a5bef16d555688edab1525c2c8b4df9
Willem van Ketwich <willvk@gmail.com>
2019-08-24 16:12:55 +1000

    add merge to dictionary. handle requests

--- spec/dictionary-spec.js
+++ spec/dictionary-spec.js
@@ -1,5 +1,30 @@
 const Dictionary = require('../lib/dictionary');
 
+let requestObject = {
+  "command": {
+    "action": "set_full_config_dictionary",
+    "value": {
+      "settings": { "setting_a": "aaaa", "setting_b": "bbbb" },
+      "variables": { "variable_c": "cccc", "variable_d": "dddd" }
+    }
+  },
+  "sender": "tide",
+  "receiver": "editor",
+};
+
+let requestObjectToAdd = {
+  "command": {
+    "action": "set_full_config_dictionary",
+    "value": {
+      "config_dictionary": {
+        "variables": { "variable_e": "eeee", "variable_f": "ffff" }
+      }
+    }
+  },
+  "sender": "tide",
+  "receiver": "editor",
+};
+
 describe('test dictionary class does as expected  ', () => {
 
   it('can have a dictionary added to it', () => {
@@ -21,6 +46,17 @@ describe('test dictionary class does as expected  ', () => {
       };
       Dictionary.set(dict);
       expect(Dictionary.getVariables()["a"]).toBe(123);
+  });
+
+  it('can retrieve add items', () => {
+      Dictionary.set(requestObject["command"]["value"]);
+      Dictionary.add(requestObjectToAdd["command"]["value"]);
+      let vars = Dictionary.getVariables();
+      let varsKeys = Object.keys(vars);
+      expect(varsKeys.indexOf('variable_c')).not.toBe(-1);
+      expect(varsKeys.indexOf('variable_d')).not.toBe(-1);
+      expect(varsKeys.indexOf('variable_e')).not.toBe(-1);
+      expect(varsKeys.indexOf('variable_f')).not.toBe(-1);
   });
 
 });
