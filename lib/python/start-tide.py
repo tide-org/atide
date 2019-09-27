00d43c72218e1fe05b1ced88a2adfda1ce1f4cc3
Willem van Ketwich <willvk@gmail.com>
2019-09-27 20:14:25 +1000

    add error handling

--- lib/python/start-tide.py
+++ lib/python/start-tide.py
@@ -1,7 +1,22 @@
-from tide import Tide
+import os
 from time import sleep
 
-sleep(1)
+try:
+    import tide
+    from tide import Tide
 
-tide = Tide()
-tide.start()
+    tide_path = os.path.abspath(tide.__file__)
+
+    print("TIDE SOURCE PATH: " + tide_path)
+
+    sleep(1)
+
+    tide = Tide()
+    tide.start()
+except Exception as e:
+    script_path = os.path.abspath(__file__)
+    error_file_path = script_path + ".error.log"
+    file_handle = open(error_file_path, "w+")
+    file_handle.write("Exception handled running start-tide: " + str(e))
+    file_handle.close()
+  
